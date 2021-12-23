95dbf0945c50557f8742d605c2af6f1354a100c7
Nenyalote <48046110+Nenyalote@users.noreply.github.com>
2021-12-24 01:42:21 +0300

    Add files via upload

---
+++ Зачет/src/Task1.java
@@ -0,0 +1,20 @@
+public class Task1 {
+  public static void main (String [] args){
+    boolean a1 = abcmath(42,5,10);
+    boolean a2 = abcmath(5,2,1);
+    boolean a3 = abcmath(1,2,3);
+
+    System.out.println(a1+" "+a2+" "+a3);
+  }
+
+  public static boolean abcmath (int a, int b, int c) {
+    int sum = a;
+    for (int i=0; i<b; i++){
+      sum = sum+sum;
+    }
+    boolean res = false;
+    if (sum%c==0)
+      res = true;
+    return res;
+  }
+}
